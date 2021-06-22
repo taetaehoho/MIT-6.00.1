3a3426fb2dfc7807278acdcd3b872e02d8c00f30
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2021-06-22 21:03:13 +0900

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math 
+
+x = int(input("Enter x here: "))
+y = int(input("Enter y here: "))
+print(x**y)
+print(math.log(x, 2))
